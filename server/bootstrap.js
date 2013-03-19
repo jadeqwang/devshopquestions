a1e3c0ac14a7ed6c2d6032bb1d28532432057611
Jade <jade@meteor.com>
2013-03-18 21:14:20 -0700

    colors and styling

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,33 +2,9 @@
 Meteor.startup(function () {
   if (Lists.find().count() === 0) {
     var data = [
-      {name: "Meteor Principles",
-       contents: [
-         ["Data on the Wire", "Simplicity", "Better UX", "Fun"],
-         ["One Language", "Simplicity", "Fun"],
-         ["Database Everywhere", "Simplicity"],
-         ["Latency Compensation", "Better UX"],
-         ["Full Stack Reactivity", "Better UX", "Fun"],
-         ["Embrace the Ecosystem", "Fun"],
-         ["Simplicity Equals Productivity", "Simplicity", "Fun"]
-       ]
-      },
-      {name: "Languages",
-       contents: [
-         ["Lisp", "GC"],
-         ["C", "Linked"],
-         ["C++", "Objects", "Linked"],
-         ["Python", "GC", "Objects"],
-         ["Ruby", "GC", "Objects"],
-         ["JavaScript", "GC", "Objects"],
-         ["Scala", "GC", "Objects"],
-         ["Erlang", "GC"],
-         ["6502 Assembly", "Linked"]
-         ]
-      },
       {name: "Questions",
        contents: [
-         ["Ada Lovelace", "Computer Science"],
+         ["I made a thing, can you look at it?", "Table 3"],
          ["Grace Hopper", "Computer Science"],
          ["Marie Curie", "Physics", "Chemistry"],
          ["Carl Friedrich Gauss", "Math", "Physics"],
